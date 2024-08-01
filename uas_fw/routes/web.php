b49ce6298e88431d49414817293c440030f63d24
putrafahrezi <145630835+putrafahrezi@users.noreply.github.com>
2024-08-01 20:55:21 +0700

    penambahan template

--- uas_fw/routes/web.php
+++ uas_fw/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/master', function () {
+    return view('master');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
