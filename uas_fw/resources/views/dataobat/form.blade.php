d3895592862091f66893469da43b211c9009a285
putrafahrezi <putrafahrezi89@gmail.com>
2024-08-04 16:18:57 +0700

    Update form.blade.php

--- uas_fw/resources/views/dataobat/form.blade.php
+++ uas_fw/resources/views/dataobat/form.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
-@section('title','Tambah Jurusan')
-@section('judul','Tambah Jurusan')
+@section('title','Tambah obat')
+@section('judul','Tambah obat')
 @section('bc')
     <ol class="breadcrumb float-sm-right">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Data Jurusan</a></li>
-        <li class="breadcrumb-item active">Tambah Jurusan</li>
+        <li class="breadcrumb-item"><a href="#">Data obat</a></li>
+        <li class="breadcrumb-item active">Tambah obat</li>
     </ol>
 @endsection
 
